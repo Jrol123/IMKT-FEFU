02e5e5954597d3fee27906c08bfc9d8883bd4191
Artemii <angap4@gmail.com>
2023-02-10 17:46:27 +1000

    StructArray_finale
    IDK why the CLion git didn't finished pushing

--- Code/I sem (CL)/Lab/L5_QueueAndStack/structArray.h
+++ Code/I sem (CL)/Lab/L5_QueueAndStack/structArray.h
@@ -1,7 +1,26 @@
 #ifndef L5_STRUCTARRAY_STRUCTARRAY_H
 #define L5_STRUCTARRAY_STRUCTARRAY_H
 
+template<typename type>
+struct Stack;
 
+template<typename type>
+Stack<type> *init(int maxSize, type a);
+
+template<typename type>
+bool deInit(Stack<type> *root);
+
+template<typename type>
+bool addHead(Stack<type> *root, type a);
+
+template<typename type>
+type top(Stack<type> *root);
+
+template<typename type>
+bool pop(Stack<type> *root);
+
+template<typename type>
+bool isEmpty(Stack<type> *root);
 
 // Queue
 
