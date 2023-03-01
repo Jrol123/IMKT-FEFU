8563306a7858b017dd0f22aedea816342a3ba513
Artemii <angap4@gmail.com>
2023-03-01 11:36:35 +1000

    update d_1_1
    Ничего важного, просто производная.
    Необходимо сначала пройтись, найти монотонность и пересечение, а затем методом дробления отрезка пополам работать

--- Code/II sem (CL)/YAIMP/d1_1_epsilonPrecis_DelOtrezok/main.cpp
+++ Code/II sem (CL)/YAIMP/d1_1_epsilonPrecis_DelOtrezok/main.cpp
@@ -8,7 +8,17 @@ const float epsilon = pow(10, -prec);
 const float a = -5;
 const float b = 5;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+double f(double x)
+{
+    return pow(x, 3) + 3 * pow(x, 2) - 9 * x - 5 + 5 * sin(x) - 5 * cos(x);
+}
+
+double df(double x)
+{
+    return 3 * pow(x, 2) + 6 * x - 9 + 5 * cos(x) + 5 * sin (x);
+} // производная
+
+int main()
+{
+
 }
