2b443a018e71afdb2855dc509f049e88b59d2f39
Artemii <angap4@gmail.com>
2023-03-02 10:03:18 +1000

    A2 - bruteforce fail
    Брутфорс не помогает.
    
    TimeLimit на 12-м тесте.
    
    Либо улучшить алгоритм, либо использовать формулу

--- Code/II sem (CL)/CATS/A2_soldiers/main.cpp
+++ Code/II sem (CL)/CATS/A2_soldiers/main.cpp
@@ -14,24 +14,25 @@ typedef struct soldier
 int main()
 {
     ifstream inf("input.txt");
-    ofstream outf("output.txt");
 
     int countOfSoldiers;
     inf >> countOfSoldiers;
     soldiers_t massSoldiers [countOfSoldiers];
-    soldiers_t finalSoldiers [countOfSoldiers];
+    int count = 0;
+//    soldiers_t finalSoldiers [countOfSoldiers];
     for(int i = 0; i < countOfSoldiers; i++)
     {
         inf >> massSoldiers[i].height;
         inf >> massSoldiers[i].name;
     }
+    inf.close();
 
     for(int index = 0; index < countOfSoldiers; index++)
     {
         float maxHeight = 0;
-        int needIndex;
+        int needIndex = -1;
 
-        for(int i = index; i < countOfSoldiers; index++)
+        for(int i = index; i < countOfSoldiers; i++)
         {
             if (maxHeight < massSoldiers[i].height)
             {
@@ -39,9 +40,21 @@ int main()
                 needIndex = i;
             }
         }
+        if (needIndex != -1)
+        {
+            for(int i = needIndex; i - index > 0; i--)
+            {
+                swap(massSoldiers[i], massSoldiers[i-1]);
+                count ++;
+            }
+        }
 
-        finalSoldiers[index] = massSoldiers[needIndex];
+//        finalSoldiers[index] = massSoldiers[needIndex];
         // bruteforce
         //Наверняка можно создать формулу
     }
+
+    ofstream outf("output.txt");
+    outf << count;
+    outf.close();
 }
