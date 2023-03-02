1935cb9fd159d617f3b43e4b823b2cb75c36cc1c
Artemii <angap4@gmail.com>
2023-03-02 14:29:25 +1000

    config and documentation added

--- Code/II sem/Analityc geometry/Projects/Console3D/lab1.py
+++ Code/II sem/Analityc geometry/Projects/Console3D/lab1.py
@@ -1,27 +1,66 @@
 import math
+import configparser
 
 
 class Point:
+    """
+    Point at VectorSpace
+
+    :module a:
+    """
     def __init__(self, x, y, z):
+        """
+        Initialisation of the point class
+
+        :parameter x: location of point by x
+        :parameter y: location of point by y
+        :parameter z: location of point by z
+        """
         self.x = x
         self.y = y
         self.z = z
 
     def distance(self, pt):
+        """
+        Distance between the self, and the pt
+
+        :param pt: some other point
+        :return: returns distance between points
+        """
         return math.sqrt((self.x - pt.x) ** 2 +
                          (self.y - pt.y) ** 2 +
                          (self.z - pt.z) ** 2)
 
     def sum(self, pt):
+        """
+        sum of two points
+
+        :param pt: some point
+        """
         self.x += pt.x
         self.y += pt.y
         self.z += pt.z
 
+    def sub(self, pt):
+        """
+        subtraction of two points
+
+        :param pt:
+        """
+        self.x -= pt.x
+        self.y -= pt.y
+        self.z -= pt.z
+
 
 class Vector:
     def __init__(self, point, x, y, z):
+        """
+        :param point:
+        :param x:
+        :param y:
+        :param z:
+        """
         self.point = point(x, y, z)
-        self.length = math.sqrt()
         self.x = x
         self.y = y
         self.z = z
@@ -31,6 +70,7 @@ class Vector:
         self.x += pt.x
         self.y += pt.y
         self.z += pt.z
+
     def length(self):
         return VectorSpace.initialPt.distance(self.point)
 
@@ -45,10 +85,12 @@ class VectorSpace:
 
 class Camera:
     def __int__(self, position, look_dir, fov, draw_distance):
+        config = configparser.ConfigParser()
+        config.read(config.cfg)
+        self.vfov = fov * (config['SCREEN_PARAM']['height'] / config['SCREEN_PARAM']['width'])
         """
-        self.vfov = fov * (h / w) - вертикальный угол наклона
-
-
+        :param fov: горизонтальный "радиус" просмотра
+        :param vfov: вертикальный "радиус" просмотра
         :param look_at: направление взгляда
         :param look_dir:
         """
@@ -61,6 +103,9 @@ class Object:
     def __init__(self, pos, rotation):
         """
         object.contains (pt) - bool
-        :param pos:
+        :param pos: position of the camera
         :param rotation:
         """
+
+
+a = Camera()
