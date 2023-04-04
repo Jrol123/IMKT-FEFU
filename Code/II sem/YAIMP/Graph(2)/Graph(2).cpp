0e6599e8ccb2a3c3b78a5c080f250e39dfef45e5
Artemii <angap4@gmail.com>
2023-04-04 21:38:33 +1000

    Проблема с нахождением точек пересечения
    Невозможно найти точки пересечения.
    Возможно @RezerdPrime сможет объяснить, как он вывел нужную функцию, потому как я не понимаю, как это сделать.

--- Code/II sem/YAIMP/Graph(2)/Graph(2).cpp
+++ Code/II sem/YAIMP/Graph(2)/Graph(2).cpp
@@ -1,16 +1,19 @@
 ﻿#include "graphics.h"
 #include <cmath>
+#include <vector>
 #pragma comment(lib,"graphics.lib")
 
+using namespace std;
+
 const float winX = 1920;
-const float winY = 1080;
+const float winY = 1200;
 
-const float scale = 125 / 2;
+const float scale = (125 - 30) / 2;
 
-const float limBX = -5;
-const float limEX = 5;
-const float limBY = -5;
-const float limEY = 5;
+const float limBX = -10;
+const float limEX = 10;
+const float limBY = -10;
+const float limEY = 10;
 
 
 int coord(float x, bool state)
@@ -70,6 +73,41 @@ void axes(float lengthBase)
     }
 }
 
+// Нахождение точек пересечения
+void intersection_points(vector<float> (&inters_pts), float f1(float), float f2(float), float eps)
+{
+    float c, a = -5, b = 5;
+    while (a < b) {
+        while (b - a > eps)
+        {
+            c = (a + b) / 2;
+
+            if ((f1(a) - f2(a)) * (f1(c) - f2(c)) <= 0)
+            {
+                b = c;
+            }
+            else
+                a = c;
+
+            // cout << c << " ";
+        }
+
+        inters_pts.push_back((a + b - eps) / 2);
+        // cout << "Solution: ";
+        // cout << (a + b - eps) / 2 << endl;
+
+        a = c + 2 * eps;
+        b = 5;
+        /*
+        while ((f1(a) - f2(a)) * (f1(b) - f2(b)) > 0 && a < b) {
+            a += 0.01;
+            b -= 0.01;
+            // cout << '(' << a << ' ' << b << ')' << endl;
+        }
+        */
+    }
+}
+
 float f1(float x)
 {
     return pow(x, 3) + 3 * pow(x, 2) - 9 * x - 5;
@@ -80,6 +118,7 @@ float f2(float x)
     return -5 * sin(x) + 5 * cos(x);
 }
 
+/*
 float f3(float x)
 {
     return sin(x) / x;
@@ -94,19 +133,21 @@ float f5(float x)
 {
     return 5;
 }
+*/
 
-void plot(float function(float), float discret, int color = WHITE)
+void plot(float f(float), float discret, int color = WHITE)
 {
     float x, y, y2, h = (limEX - limBX) / discret;
     // cout << h;
     setcolor(color);
     for (x = limBX; x < limEX; x += h)
     {
-        y = function(x);
-        y2 = function(x + h);
+        y = f(x);
+        y2 = f(x + h);
         // cout << y << endl;
         if (limBY <= y && y <= limEY &&
-            limBY <= y2 && y2 <= limEY) {
+            limBY <= y2 && y2 <= limEY)
+        {
             line(coord(x, true), coord(y, false), coord(x + h, true), coord(y2, false));
         }
     }
@@ -117,7 +158,9 @@ int main()
     float x, y, z;
     initwindow(winX, winY); // открыть окно для графики
 
-    int const discr = pow(10, 5);
+    int const discr = pow(10, 4);
+
+    // Рамки
     setcolor(LIGHTRED);
     line(coord(-5, true), coord(-5, false), coord(-5, true), coord(5, false));
     line(coord(5, true), coord(-5, false), coord(5, true), coord(5, false));
@@ -125,12 +168,19 @@ int main()
     line(coord(5, true), coord(5, false), coord(-5, true), coord(5, false));
     setcolor(WHITE);
 
+    // Оси
     axes(0.1);
-    plot(f1, discr, WHITE);
+    // Графики
+    plot(f1, discr, LIGHTGREEN);
     plot(f2, discr, YELLOW);
+    /*
     plot(f3, discr, LIGHTGREEN);
     plot(f4, discr, LIGHTBLUE);
     //plot(f5, discr, LIGHTRED);
+    */
+    // Точки пересечения
+    vector<float> interPoints;
+    intersection_points(interPoints, f1, f2, 0.01);
 
     setbkcolor(BLACK); // Фон
 
