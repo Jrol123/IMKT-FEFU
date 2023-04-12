644c0f6493c396ea94e0fbfd7e7c4cf084dcee86
Artemii <angap4@gmail.com>
2023-04-12 11:14:53 +1000

    d3_monolinearList
    Реализовал добавление элемента.
    Но код ломается (не знаю, почему. Идей нет)

---
+++ Code/II sem/YAIMP/d3_monolinearList/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+//struct Data
+//{
+//    int val;
+//
+//    bool operator>(Data other)
+//    {
+//         return (this->val > other.val);
+//    }
+//};
+
+struct Node
+{
+//    Data val;
+    int val;
+    Node * next = nullptr;
+};
+
+struct List
+{
+    Node * first = nullptr;
+
+    List(int val)
+    {
+        this-> first = new Node (val);
+    }
+
+    void push (int val)
+    {
+        Node * prevNode = first;
+        if (first->val > val)
+        {
+            this-> first = new Node (val);
+            this->first-> next = prevNode;
+            return;
+        } // Добавление элемента слева
+        while (prevNode-> next != nullptr)
+        {
+            if (prevNode->next->val > val)
+            {
+                prevNode->next = new Node(val, prevNode->next);
+            }
+            prevNode = prevNode-> next;
+        }
+        prevNode-> next = new Node (val);
+   } // Неубывающий список
+};
+
+int main()
+{
+    int countElements;
+    cin >> countElements;
+
+    int element;
+    cin >> element;
+    List testList(element);
+    for(int i = 1; i < countElements; i++)
+    {
+        cin >> element;
+        cout << element;
+        testList.push(element);
+    }
+    cout << 1;
+
+//    List testList(Data(1));
+//    testList.push(0);
+//    testList.push(2);
+}
