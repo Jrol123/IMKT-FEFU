7d08b49d510c9f6ea389101705e9089de0be659f
Artemii <angap4@gmail.com>
2023-04-13 15:37:02 +1000

    commit

--- Code/II sem/YAIMP/KR_Optimization/main.cpp
+++ Code/II sem/YAIMP/KR_Optimization/main.cpp
@@ -14,14 +14,25 @@
 #include <iostream>
 using namespace std;
 
+void solution(int costs[3][5], int ways[3][5],
+              int matrixLoad [3], int matrixLim [5],
+              int len, int nowIndex = 0, int index = 0)
+{
+    ways[index][nowIndex] += 1;
+}
+
 int main()
 {
-    int matrixCost [15] =
+    int matrixCost [3][5] =
             {2, 4,	1,	6,	7,
             3,	3,	5,	4,	2,
             8,	9,	6,	3,	4};
-    int matrixWay [15] {0};
+    int matrixWay [3][5] {0};
     int loadA1 = 120, loadA2 = 110, loadA3 = 130;
-    int limB1 = 80, libB2 = 60, limB3 = 70, limB4 = 100, limB5 = 50;
-    // реализовать перебором
+    int limB1 = 80, limB2 = 60, limB3 = 70, limB4 = 100, limB5 = 50;
+    int massLoad[3] = {loadA1, loadA2, loadA3};
+    int massLim[5] = {limB1, limB2, limB3, limB4, limB5};
+    int cost = INT_MAX;
+
+    solution(matrixCost, matrixWay, massLoad, massLim, 15);
 }
