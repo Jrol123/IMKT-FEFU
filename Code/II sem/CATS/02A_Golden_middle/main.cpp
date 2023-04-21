ddedb4ad16ebecfab37726eefc4ec917e732d3c6
Artemii <angap4@gmail.com>
2023-04-21 22:40:32 +1000

    02A_Golden_middle Просто `qsort` не работает.
    Необходимо заморочиться с кучей. Но как?

--- Code/II sem/CATS/02A_Golden_middle/main.cpp
+++ Code/II sem/CATS/02A_Golden_middle/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+//#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int compare(const void * x1, const void * x2)   // функция сравнения элементов массива
+{
+    return ( *(int*)x1 - *(int*)x2 );              // если результат вычитания равен 0, то числа равны, < 0: x1 < x2; > 0: x1 > x2
+}
+
+// Неправильно сортирует при std::qsort
+// Использование сразу двух куч?
+int main()
+{
+    ifstream inf("input.txt");
+    int count;
+    inf >> count;
+    vector<int> massNums(count);
+//    int massNums[count];
+    ofstream outf("output.txt");
+    for(int index = 0; index < count; index ++)
+    {
+        inf >> massNums[index];
+        qsort(&massNums[0], index + 1, sizeof(int), compare);
+        // Каждый раз сортирует по-разному!!!
+        outf << massNums[index / 2] << " ";
+//        cout << massNums[index / 2] << " ";
+    }
+    inf.close();
+    outf.close();
 }
