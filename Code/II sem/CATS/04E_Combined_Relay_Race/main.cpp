78662063a926a5bb8eb3260f61a39e212cf2aca9
Artemii <angap4@gmail.com>
2023-04-30 08:50:13 +1000

    04E_Combined_Relay_Race 25/25
    Полностью готовое решение.

--- Code/II sem/CATS/04E_Combined_Relay_Race/main.cpp
+++ Code/II sem/CATS/04E_Combined_Relay_Race/main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <fstream>
+/*
+ * Основная идея заключается в том, чтобы менять местами не участников, а дисциплины,
+ * тем самым, вместо перебора участников, можно всегда брать элемент line[indexFirstChar][indexFirstChar] и не беспокоиться.
+ */
 
 const unsigned short countDisciplines = 4;
 const unsigned short countParticipants = 4;
+
+void transpose(float matrix[countParticipants][countDisciplines])
+{
+    float t;
+    for(int i = 0; i < countParticipants; ++i)
+    {
+        for(int j = i; j < countDisciplines; ++j)
+        {
+            t = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = t;
+        }
+    }
+}
 
 void permutations(float (&line)[countParticipants][countDisciplines], int (&finalRes)[countParticipants], int (&res)[countParticipants],
                   unsigned short indexLastChar, float &minTime, float time = 0, unsigned short indexFirstChar = 0)
 {
     if(indexFirstChar == indexLastChar)
     {
-        if(minTime > time + line[3][3])
+        if(time + line[3][3] < minTime)
         {
-            minTime = time;
+            minTime = time + line[3][3];
             for(int i = 0; i < countParticipants; i++)
             {
                 finalRes[i] = res[i];
@@ -22,21 +40,19 @@ void permutations(float (&line)[countParticipants][countDisciplines], int (&fina
     {
         for (unsigned short index = indexFirstChar; index <= indexLastChar; index++)
         {
-            int j = index;
-            for(int i = index - 1; i >= indexFirstChar; i--)
+            if(index > indexFirstChar)
             {
-                std::swap(line[i], line[j]);
-                std::swap(res[i], res[j]);
-                j --;
-            } // Прокручивание всех цифр для лексикографического порядка
+                std::swap(line[indexFirstChar], line[index]);
+                std::swap(res[indexFirstChar], res[index]);
+            }
+
             permutations(line, finalRes, res, indexLastChar, minTime, time + line[indexFirstChar][indexFirstChar], indexFirstChar + 1);
-            j = indexFirstChar;
-            for(int i = indexFirstChar + 1; i<= index; i++)
+
+            if(index > indexFirstChar)
             {
-                std::swap(line[i], line[j]);
-                std::swap(res[i], res[j]);
-                j ++;
-            } // Реверс прокручивание
+                std::swap(res[indexFirstChar], res[index]);
+                std::swap(line[indexFirstChar], line[index]);
+            }
         }
     }
 }
@@ -62,12 +78,13 @@ int main()
     int finalRes[countParticipants]{1, 2, 3, 4};
     int res[countParticipants]{1, 2, 3, 4};
 
+    transpose(massTime);
     permutations(massTime, finalRes, res, countParticipants - 1, minTime);
 
     std::ofstream outf ("output.txt");
     for(int index : finalRes)
     {
-        outf << index;
+        outf << index << " ";
     }
     outf.close();
 
