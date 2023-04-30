d058002fdf9aaf5d538c7b6e421397c99ce3d248
Artemii <angap4@gmail.com>
2023-04-30 10:58:28 +1000

    02A_Golden_middle 15/15

--- Code/II sem/CATS/02A_Golden_middle/main.cpp
+++ Code/II sem/CATS/02A_Golden_middle/main.cpp
@@ -1,31 +1,60 @@
 //#include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-int compare(const void * x1, const void * x2)   // функция сравнения элементов массива
-{
-    return ( *(int*)x1 - *(int*)x2 );              // если результат вычитания равен 0, то числа равны, < 0: x1 < x2; > 0: x1 > x2
-}
-
-// Неправильно сортирует при std::qsort
-// Использование сразу двух куч?
 int main()
 {
     ifstream inf("input.txt");
-    int count;
-    inf >> count;
-    vector<int> massNums(count);
-//    int massNums[count];
+    int countOfElements;
+    inf >> countOfElements;
+    priority_queue<int, vector<int>, less<int>> maxHeap; // Левая половина
+    priority_queue<int, vector<int>, greater<int>> minHeap; // Правая половина
+
     ofstream outf("output.txt");
-    for(int index = 0; index < count; index ++)
+
+    int element;
+    inf >> element;
+    maxHeap.push(element);
+    outf << element << endl;
+
+    for(int index = 1; index < countOfElements; ++index)
     {
-        inf >> massNums[index];
-        qsort(&massNums[0], index + 1, sizeof(int), compare);
-        // Каждый раз сортирует по-разному!!!
-        outf << massNums[index / 2] << " ";
-//        cout << massNums[index / 2] << " ";
+        inf >> element;
+
+        if(maxHeap.size() > minHeap.size())
+        {
+            if(element < maxHeap.top())
+            {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+                maxHeap.push(element);
+            } // Балансировка (т. к. будет перекос на 2 элемента)
+            else
+            {
+                minHeap.push(element);
+            }
+            outf << maxHeap.top() << endl;
+        } // Если слева кол-во элементов больше на 1
+        else
+        {
+            if(element > minHeap.top())
+            {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+                minHeap.push(element);
+            } // Балансировка (т. к. будет перекос на 2 элемента)
+            // Проблем с перекосом не будет, т. к. даже если количество элементов одинаковое,
+            // то на следующем цикле всё зайдёт в верхний if
+            else
+            {
+                maxHeap.push(element);
+            }
+            outf << maxHeap.top() << endl;
+        } // Если справа <= элементов, чем слева
     }
+
     inf.close();
     outf.close();
 }
