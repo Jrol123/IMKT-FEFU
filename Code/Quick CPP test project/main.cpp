8978f840f9c8d45900c874eafdf7a0b28f443511
Artemii <angap4@gmail.com>
2023-05-02 22:53:25 +1000

    Quick CPP

--- Code/Quick CPP test project/main.cpp
+++ Code/Quick CPP test project/main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-struct Node
+int f(vector<int>& stones)
 {
-    int val;
-    Node *next, *prev;
+    // Convert stones vector to a max heap
+    priority_queue<int> max_heap(stones.begin(), stones.end());
 
-    void push_back()
+    // Smash stones until there is only one left
+    while (max_heap.size() > 1)
     {
-        Node *buff = new Node;
+        int x = max_heap.top();
+        max_heap.pop();
+        int y = max_heap.top();
+        max_heap.pop();
+        if (x != y)
+        {
+            max_heap.push(y - x);
+        }
     }
-};
+
+    // Return the last stone's weight (if there is one)
+    return max_heap.empty() ? 0 : max_heap.top();
+}
 
 int main()
 {
     ifstream inf("input.txt");
     ofstream outf("output.txt");
 
-    Node c = Node(11);
+    vector<int> vector1 {2,7,4,1,8,1};
 
+    cout << f(vector1);
 
+    return 0;
 }
